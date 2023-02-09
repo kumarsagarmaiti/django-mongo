22e76b8c7c80bfbb2cb7cd2b13cba1a36457e81e
KingK9t8 <ksagarm789@gmail.com>
2023-02-09 18:50:56 +0530

    added validation for missing fields

--- flask-project/app.py
+++ flask-project/app.py
@@ -34,26 +34,32 @@ def add_employee():
 
 @app.route("/employees", methods=["GET"])
 def get_all_employees():
-    employees = employee_collection.find({})
-    return json.loads(dumps(employees))
+    try:
+        employees = employee_collection.find({})
+        return json.loads(dumps(employees))
+    except Exception as e:
+        return Response(f"An error occured {e}")
 
 
 @app.route("/employee/<objectid>", methods=["GET", "PUT", "DELETE"])
 def employee_by_id(objectid):
-    find_by_id = {"_id": ObjectId(objectid)}
-    employee = employee_collection.find_one(find_by_id)
-    if employee is None:
-        abort(code=404)
-    if request.method == "GET":
-        return json.loads(dumps(employee))
+    try:
+        find_by_id = {"_id": ObjectId(objectid)}
+        employee = employee_collection.find_one(find_by_id)
+        if employee is None:
+            abort(code=404)
+        if request.method == "GET":
+            return json.loads(dumps(employee))
 
-    if request.method == "PUT":
-        employee_collection.update_one(find_by_id, {"$set": request.get_json()})
-        return "Employee updated successfully"
+        if request.method == "PUT":
+            employee_collection.update_one(find_by_id, {"$set": request.get_json()})
+            return "Employee updated successfully"
 
-    if request.method == "DELETE":
-        employee_collection.delete_one(find_by_id)
-        return "Employee deleted successfully"
+        if request.method == "DELETE":
+            employee_collection.delete_one(find_by_id)
+            return "Employee deleted successfully"
+    except Exception as e:
+        return Response(f"An error occured {e}")
 
 
 if __name__ == "_main_":
